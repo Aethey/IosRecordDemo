070263ee737c1bd3d463d20f9fb7ca220df2443c
Ry <pandora369741@yahoo.co.jp>
2021-08-06 14:57:22 +0900

    Initial Commit

---
+++ IosRecordDemo/IosRecordDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosRecordDemoApp.swift
+//  IosRecordDemo
+//
+//  Created by Y Ryu on 2021/08/06.
+//
+
+import SwiftUI
+
+@main
+struct IosRecordDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
